cc17462bd71bd0b729ce9ad59101832da9cd9d1d
Dixit Patel <dixit.milkybar@gmail.com>
2018-03-21 12:34:32 -0600

    spring rec_center

---
+++ src/main/java/com/recmgmt/RecmgmtApplication.java
@@ -0,0 +1,12 @@
+package com.recmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecmgmtApplication.class, args);
+	}
+}
